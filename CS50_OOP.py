48356bc388832df57e602108dd1d1dc428d63025
antony oduor <116315398+oduoranto@users.noreply.github.com>
2024-06-17 23:47:47 +0300

    Classes and methods

---
+++ CS50_OOP.py
@@ -0,0 +1,15 @@
+# class : 
+class point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def sq(self,):
+        
+        return self.x * self.y
+
+
+p = point(28,6)
+print(p.x)
+print(p.y)
+print(p.sq())       
