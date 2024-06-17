10346e575f6f12deb8a56b6f32accc666df97b76
antony oduor <116315398+oduoranto@users.noreply.github.com>
2024-06-17 23:48:20 +0300

    Decoraters

---
+++ Decoraters.py
@@ -0,0 +1,13 @@
+def announce(f):
+    def wrapper():
+        print("About to run a function..")
+        f()
+        print("Done..")
+
+    return wrapper    
+
+@announce
+def hello():
+    print("Hello World")
+
+hello()    
