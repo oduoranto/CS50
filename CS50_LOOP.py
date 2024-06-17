85a70cba13f2787a8d258f5f31372f42f5ca243a
antony oduor <116315398+oduoranto@users.noreply.github.com>
2024-06-17 23:46:23 +0300

    Loops

---
+++ CS50_LOOP.py
@@ -0,0 +1,15 @@
+number = 20
+
+for n  in range(number):
+    print(n)
+
+for i in [0,1,2,3,4,5]:
+    print(i)
+
+# Dictionary
+
+names = {"F_name": "Anto", "S_name" : "Oduor", "Age": 21}    
+
+print(names["Age"])
+for keys in names :
+    print(names[keys]
